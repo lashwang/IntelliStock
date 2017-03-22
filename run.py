7504b1d20a96b57c67e67e18b3c16e5016f62679
simon <lashwang@outlook.com>
2017-03-22 23:39:27 +0800

    次新股分析(2)

--- run.py
+++ run.py
@@ -9,25 +9,29 @@ import pandas as pd
 
 class StockReport(object):
 
-    global writer
-    global new_stock_list
-
     def read_baisc_stock_from_file(self):
         df = pd.read_csv('data/all.csv', dtype={'code':'object'})
         df.set_index('code')
         return df
 
-    def __init__(self):
-        writer = pd.ExcelWriter('stock.xlsx', engine='xlsxwriter')
-        df = self.read_baisc_stock_from_file()
+    def read_baisc_stock_from_file(self):
+        df = ts.get_stock_basics()
         
+
+
+    def __init__(self):
+        self.writer = pd.ExcelWriter('stock.xlsx', engine='xlsxwriter')
+        df = self.read_baisc_stock_from_file()
+        self.new_stock_list = df.filter(items=['code','outstanding','totals','timeToMarket'])
+        self.new_stock_list.set_index('code')
+
 
 
 
 
     def get_new_stock_report(self):
-        print "loading..."
-
+        self.new_stock_list.to_excel(self.writer, sheet_name='Sheet1')
+        self.writer.save()
 
 def main():
     fire.Fire(StockReport)
