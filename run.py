25e91acaf31b59bba108ce7461eecddf16f2a4ed
simon <lashwang@outlook.com>
2017-03-23 11:00:23 +0800

    次新股报表

--- run.py
+++ run.py
@@ -14,8 +14,9 @@ class StockReport(object):
         df.set_index('code')
         return df
 
-    def read_baisc_stock_from_file(self):
+    def read_baisc_stock_from_network(self):
         df = ts.get_stock_basics()
+        return df
         
 
 
@@ -23,14 +24,26 @@ class StockReport(object):
         self.writer = pd.ExcelWriter('stock.xlsx', engine='xlsxwriter')
         df = self.read_baisc_stock_from_file()
         self.new_stock_list = df.filter(items=['code','outstanding','totals','timeToMarket'])
-        self.new_stock_list.set_index('code')
+
+
 
 
 
 
 
     def get_new_stock_report(self):
-        self.new_stock_list.to_excel(self.writer, sheet_name='Sheet1')
+        # 未上市新股
+        self.stokc_untrade = self.new_stock_list[self.new_stock_list.timeToMarket == 0]
+        self.stokc_untrade.to_excel(self.writer, sheet_name=u'未上市新股',encoding='GBK')
+
+        # 筛选最近一年上市的新股
+        df = self.new_stock_list[self.new_stock_list.timeToMarket > 20160801]
+        df.to_excel(self.writer, sheet_name=u'次新股',encoding='GBK')
+
+
+
+
+
         self.writer.save()
 
 def main():
