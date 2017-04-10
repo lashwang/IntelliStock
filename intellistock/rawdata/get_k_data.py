44e969be0484c661462aaa30d15538cf04e77515
simon <lashwang@outlook.com>
2017-04-10 11:02:02 +0800

    除权复权节点解析(1)

--- intellistock/rawdata/get_k_data.py
+++ intellistock/rawdata/get_k_data.py
@@ -3,6 +3,9 @@
 
 import logging
 from intellistock.http_cache import HttpCache
+from lxml import etree
+import StringIO
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -14,16 +17,31 @@ class GetKData(object):
     def __init__(self):
         pass
 
+    @classmethod
+    def text(cls,elt):
+        return elt
 
     @classmethod
     def get_FHPG_info(cls,code=None):
+        '''
+        获得分红配股信息
+        '''
         if code is None:
             logger.error('the stock code is empty')
             return None
 
         url = cls.FHPG_URL.format(code)
+        html_raw = HttpCache().Request(url)
+        parser = etree.HTMLParser()
+        html = etree.parse(StringIO.StringIO(html_raw),parser=parser)
+        table = html.xpath("/html/body[@class='test']/div[@class='area']/div[@class='inner_box'][1]/table[@class='table_bg001 border_box limit_sale']")
+        logger.debug(len(table))
+        table = table[0]
+        logger.debug(table)
+        logger.debug(type(table))
+        logger.debug(table.xpath("//thead"))
 
-        html = HttpCache().Request(url)
 
-        logger.debug(html)
+
+
 
