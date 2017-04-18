9800c8456aba595a9eef56605530d672f778565e
simon <lashwang@outlook.com>
2017-04-19 00:07:06 +0800

    新股数据业务逻辑整理

--- intellistock/data_wrapper.py
+++ intellistock/data_wrapper.py
@@ -18,6 +18,8 @@ from intellistock.trade.get_basic_info import GetBasicInfo
 from intellistock.trade.trade_checking import TradeChecking
 from intellistock.trade.get_new_stock_list import NewStockData
 
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,15 +30,20 @@ class DataWrapper:
     def interface_test(self):
         TradeChecking.check_is_trading()
         TradeChecking.get_all_trade_cal()
-        stock_list = ['000001','300619','300414']
+        stock_list = ['000002','300619','300414', '600519', '002839', '603039']
         for _code in stock_list:
             #GetBasicInfo.get_FHPG_info(_code)
             #GetBasicInfo.get_GBJG_info(_code)
             pass
 
-        NewStockData.get_new_stock_data()
+        #NewStockData.get_new_stock_data()
 
 
+        for _code in stock_list:
+            start_day = "2016-04-08"
+            end_day = arrow.now().format("YYYY-MM-DD")
+            GetKData.get_k_data_by_day(code=_code,k_type=GetKData.KDayType.DAY,
+                                       start_day=start_day, end_day=end_day,fq_type=GetKData.FQType.QFQ)
 
 
 
