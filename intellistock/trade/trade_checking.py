b3f6bea58089b7449dbac9b69f6a802e3408093e
simon <lashwang@outlook.com>
2017-05-10 15:30:23 +0800

    add trade checking test code.

--- intellistock/trade/trade_checking.py
+++ intellistock/trade/trade_checking.py
@@ -6,42 +6,96 @@ import ast
 import arrow
 
 from intellistock.trade import *
+from operator import itemgetter
+
 
 logger = logging.getLogger(__name__)
 
 
 class StockCalDay(SpiderBase):
     name = __name__
+    YEAR_START = 2005
     CAL_DAY_URL = 'http://vaserviece.10jqka.com.cn/mobilecfxf/data/json_{year}.txt'
 
-    def __init__(self,cal):
-        self._cal = arrow.get(cal, 'YYYY-MM-DD')
-        super(StockCalDay, self).__init__()
-
-        self.cache_timeout = 7*24
+    def __init__(self,**kwargs):
+        super(StockCalDay, self).__init__(**kwargs)
+        self.cache_timeout = 30 * 24
+        self.year_start = self.cls.YEAR_START
+        self.year_index = self.year_start
+        self.year_end = arrow.now().year
+        self.cal_list = {}
 
 
     def _get_start_url(self):
-        url = self.cls.CAL_DAY_URL.format(year=self._cal.year)
+        url = self.cls.CAL_DAY_URL.format(year=self.year_start)
         return url
 
 
     def _parse(self, data):
-        self._cal_list = ast.literal_eval(data)
-        self._trade_day = self._check_trading_day()
+        cal_list = ast.literal_eval(data)
+        self.cal_list[self.year_index] = cal_list
+        self.year_index = self.year_index + 1
+        if self.year_index > self.year_end:
+            return
 
-    def _check_trading_day(self):
-        date_str = self._cal.format('MMDD')
+        return self.cls.CAL_DAY_URL.format(year=self.year_index)
 
-        if date_str in self._cal_list:
+    def _on_parse_finished(self):
+        pass
+
+
+    def _parser_date_param(self,date):
+        if date is None:
+            a = arrow.now()
+        else:
+            a = arrow.get(date,'YYYY-MM-DD')
+
+        return a
+
+    def check_trading_day(self,date=None):
+        '''
+        check if the given date is the trading day.
+        :param date:YYYY-MM-DD, 
+        :return: 
+        '''
+
+
+        a = self._parser_date_param(date)
+
+        date_str = a.format('MMDD')
+        if date_str in self.cal_list[a.year]:
             return True
-
         return False
 
-    @run_once
-    def is_trading_day(self):
-        return self._trade_day
+    def get_last_trading_day(self,date=None):
+        a = self._parser_date_param(date)
+        #a = a.replace(days=-1)
+        year = a.year
+        mmdd = None
 
+        date_str = a.format('MMDD')
+
+        if date_str <= self.cal_list[a.year][0]:
+            year = a.year - 1
+            mmdd = self.cal_list[a.year - 1][-1]
+
+        if date_str > self.cal_list[a.year][-1]:
+            mmdd = self.cal_list[a.year][-1]
+
+
+        for index,key in enumerate(self.cal_list[year]):
+            if key >= date_str:
+                mmdd = self.cal_list[year][index-1]
+                break
+
+        if mmdd is None:
+            raise ValueError
+
+        return "{}-{}-{}".format(year,mmdd[0:2],mmdd[2:4])
+
+
+    def get_cal_list(self):
+        return self.cal_list
 
 
 
---
+++ tests/test_trade_checking.py
@@ -0,0 +1,23 @@
+import unittest2
+from intellistock.trade.trade_checking import StockCalDay
+import arrow
+
+
+class MyTestCase(unittest2.TestCase):
+    def test_cal_day(self):
+        stock_cal_day = StockCalDay()
+        stock_cal_day.load_data()
+
+        cal_list = stock_cal_day.get_cal_list()
+        self.assertTrue(len(cal_list) >= 13)
+
+        a = arrow.get('2017-01-01', 'YYYY-MM-DD')
+        while True:
+            if_trade_day = stock_cal_day.check_trading_day(a.format('YYYY-MM-DD'))
+            last_trade_day = stock_cal_day.get_last_trading_day(a.format('YYYY-MM-DD'))
+            a = a.replace(days=+1)
+            if a.year > 2017:
+                break;
+
+if __name__ == '__main__':
+    unittest2.main()
